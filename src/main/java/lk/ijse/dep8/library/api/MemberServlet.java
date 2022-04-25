3a7c715fc6a94c688120d369c81d25348d5840f5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-04-25 16:29:15 +0530

    Handle duplicate members

--- src/main/java/lk/ijse/dep8/library/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep8/library/api/MemberServlet.java
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @WebServlet(name = "MemberServlet", value = {"/members", "/members/"})
@@ -43,7 +44,15 @@ public class MemberServlet extends HttpServlet {
             }
 
             try (Connection connection = pool.getConnection()) {
-                PreparedStatement stm = connection.prepareStatement("INSERT INTO member (nic, name, contact) VALUES (?,?,?)");
+                PreparedStatement stm = connection.prepareStatement("SELECT * FROM member WHERE nic=?");
+                stm.setString(1,member.getNic());
+                ResultSet rst = stm.executeQuery();
+                if (rst.next()){
+                    response.sendError(HttpServletResponse.SC_CONFLICT, "Member already exists");
+                    return;
+                }
+
+                stm = connection.prepareStatement("INSERT INTO member (nic, name, contact) VALUES (?,?,?)");
                 stm.setString(1, member.getNic());
                 stm.setString(2, member.getName());
                 stm.setString(3, member.getContact());
