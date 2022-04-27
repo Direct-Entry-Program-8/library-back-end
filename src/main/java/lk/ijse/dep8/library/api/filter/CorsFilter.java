593e61b6f27bb603d9e44ba1c5ef0a4d91711646
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-04-27 15:14:51 +0530

    Add missed filter chain executor

--- src/main/java/lk/ijse/dep8/library/api/filter/CorsFilter.java
+++ src/main/java/lk/ijse/dep8/library/api/filter/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         String origin = req.getHeader("Origin");
-        if (origin.toLowerCase().startsWith(getServletContext().getInitParameter("origin"))) {
+        if (origin.toLowerCase().contains(getServletContext().getInitParameter("origin"))) {
             res.setHeader("Access-Control-Allow-Origin", origin);
             if (req.getMethod().equals("OPTIONS")) {
                 res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE, HEAD");
@@ -21,5 +21,6 @@ public class CorsFilter extends HttpFilter {
                 res.setHeader("Access-Control-Expose-Headers", "Content-Type");
             }
         }
+        chain.doFilter(req,res);
     }
 }
